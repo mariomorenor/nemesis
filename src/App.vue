950437050290f803ab005d12b6075092e83dc160
Jonathan Moreno <mario_morenor@hotmail.com>
2023-07-14 19:45:58 -0500

    Inicializacion de Variables

--- src/App.vue
+++ src/App.vue
@@ -6,4 +6,53 @@
 
 <script setup lang="ts">
 import { IonApp, IonRouterOutlet } from '@ionic/vue';
+import { onBeforeMount } from 'vue';
+
+// Storage
+import { Storage } from '@ionic/storage';
+const store = new Storage();
+let storage: Storage;
+
+// Init
+
+function init(arr: { key: string, value: any }[], storageKeys: string[]) {
+  arr.forEach(async (item) => {
+    if (!storageKeys.includes(item.key)) {
+      await storage.set(item.key, item.value);
+    }
+  })
+}
+
+// Hooks
+onBeforeMount(async () => {
+  storage = await store.create();
+
+  const keys: string[] = await storage.keys();
+
+  init(values, keys);
+
+});
+
+// Local Store 'Add local inital keys to set in storage'
+const values = [
+  {
+    key: 'SERVER',
+    value: {
+      url: '',
+      database: ''
+    }
+  },
+  {
+    key: 'LOGIN',
+    value: false
+  },
+  {
+    key: 'USER',
+    value: {}
+  }
+
+];
+
+
+
 </script>
