f3c97fd9f810532ade63ddf35d1b0f72b465da6b
Jonathan Moreno <mario_morenor@hotmail.com>
2023-07-15 23:02:11 -0500

    Vista Attendance

---
+++ src/views/AttendancePage.vue
@@ -0,0 +1,54 @@
+<template>
+    <ion-page>
+        <main-menu></main-menu>
+        <ion-page id="main-content">
+            <ion-header>
+                <ion-toolbar>
+                    <ion-buttons>
+                        <ion-menu-button></ion-menu-button>
+                        <ion-label>Asistencias</ion-label>
+                    </ion-buttons>
+                </ion-toolbar>
+            </ion-header>
+            <ion-content>
+                <ion-list>
+                    <ion-item>
+                        <ion-label>Fecha Inicio:</ion-label>
+                        <ion-datetime-button datetime="datetime"></ion-datetime-button>
+
+                        <ion-modal :keep-contents-mounted="true">
+                            <ion-datetime id="datetime"></ion-datetime>
+                        </ion-modal>
+                    </ion-item>
+                    <ion-item>
+                        <ion-label>Fecha Fin</ion-label>
+                        <ion-datetime-button datetime="datetime2"></ion-datetime-button>
+
+                        <ion-modal :keep-contents-mounted="true">
+                            <ion-datetime id="datetime2"></ion-datetime>
+                        </ion-modal>
+                    </ion-item>
+                </ion-list>
+
+                <ion-refresher slot="fixed" @ionRefresh="handleRefresh($event)">
+                    <ion-refresher-content>
+
+                    </ion-refresher-content>
+                </ion-refresher>
+            </ion-content>
+        </ion-page>
+
+    </ion-page>
+</template>
+
+<script lang="ts" setup>
+import { IonPage, IonHeader, IonToolbar, IonLabel, IonContent, IonButtons, IonMenuButton, IonRefresher, IonRefresherContent, IonCard, IonCardContent, IonItem, IonDatetimeButton, IonModal, IonDatetime, IonList } from '@ionic/vue';
+import MainMenu from '@/components/MainMenu.vue';
+
+
+function handleRefresh(event: any) {
+
+    event.target.complete();
+}
+
+</script>
