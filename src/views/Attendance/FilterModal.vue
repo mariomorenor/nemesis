6e25e6a852a5f4c12eb9c9dbe4fbf32aaa36b336
Jonathan Moreno <mario_morenor@hotmail.com>
2023-07-17 22:12:54 -0500

    Restricciones de Fechas

--- src/views/Attendance/FilterModal.vue
+++ src/views/Attendance/FilterModal.vue
@@ -17,16 +17,18 @@
                     <ion-label>Fecha Inicio:</ion-label>
                     <ion-datetime-button datetime="start-date"></ion-datetime-button>
                     <ion-modal :show-backdrop="true" :backdrop-dismiss="false" :keep-contents-mounted="true">
-                        <ion-datetime v-model="props.dates.startDate" presentation="date" :show-default-buttons="true"
-                            done-text="Confirmar" cancel-text="Cancelar" id="start-date"></ion-datetime>
+                        <ion-datetime v-model="props.dates.startDate" @ion-change="changeDate()" :max="maxDate"
+                            presentation="date" :show-default-buttons="true" done-text="Confirmar" cancel-text="Cancelar"
+                            id="start-date"></ion-datetime>
                     </ion-modal>
                 </ion-item>
                 <ion-item>
                     <ion-label>Fecha Fin:</ion-label>
                     <ion-datetime-button datetime="end-date"></ion-datetime-button>
                     <ion-modal :backdrop-dismiss="false" :keep-contents-mounted="true">
-                        <ion-datetime v-model="props.dates.endDate" presentation="date" :show-default-buttons="true"
-                            done-text="Confirmar" cancel-text="Cancelar" id="end-date"></ion-datetime>
+                        <ion-datetime v-model="props.dates.endDate" @ion-change="changeDate()" :min="minDate"
+                            presentation="date" :show-default-buttons="true" done-text="Confirmar" cancel-text="Cancelar"
+                            id="end-date"></ion-datetime>
                     </ion-modal>
                 </ion-item>
             </ion-list>
@@ -36,6 +38,7 @@
 
 <script lang="ts" setup>
 import { IonModal, IonHeader, IonToolbar, IonButtons, IonButton, IonTitle, IonContent, IonItem, IonLabel, IonInput, IonDatetimeButton, IonDatetime, IonList } from '@ionic/vue';
+import moment from 'moment';
 import { ref } from 'vue';
 
 const props = defineProps(['showModal', 'dates'])
@@ -47,9 +50,17 @@ function cancel() {
 
 function confirm() {
     emit('closeModal', { dates: props.dates });
-
 }
 
+
+var minDate = ref(moment().startOf('month').format('Y-MM-DD'));
+var maxDate = ref(moment().format('Y-MM-DD'));
+
+function changeDate() {
+
+    minDate = props.dates.startDate;
+    maxDate = props.dates.endDate;
+}
 
 
 </script>
