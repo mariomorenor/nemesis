c2afa08a07c95fda47f8bb01d1128a0e5b39a4e1
Jonathan Moreno <mario_morenor@hotmail.com>
2023-07-17 22:01:54 -0500

    Visualizacion de Horas

--- src/views/Attendance/AttendancePage.vue
+++ src/views/Attendance/AttendancePage.vue
@@ -4,7 +4,7 @@
         <filter-modal :dates="dates" :showModal="showFilterModal" @closeModal="setFilters"></filter-modal>
         <ion-page id="main-content">
             <ion-header>
-                <ion-toolbar>
+                <ion-toolbar color="primary">
                     <ion-buttons>
                         <ion-menu-button></ion-menu-button>
                         <ion-label>Asistencias</ion-label>
@@ -17,10 +17,32 @@
                 </ion-toolbar>
             </ion-header>
             <ion-content>
-                <ion-list>
-                    <ion-item v-for="attendace in attendaces">
-                        <ion-label><strong>Marcación:</strong> {{ attendace.fecha_hora }}</ion-label>
-                    </ion-item>
+                <ion-list lines="full">
+                    <template v-if="attendances.length > 0">
+                        <ion-item-sliding v-for="attendance in attendances">
+                            <ion-item>
+                                <ion-label class="item-attendance">
+                                    <h2><strong>Fecha:</strong> {{ attendance.fecha }} <small>{{ attendance.day }}</small>
+                                    </h2>
+                                    <ion-chip color="primary" v-for="record in attendance.records">{{ record.fecha_hora
+                                    }}</ion-chip>
+                                </ion-label>
+                            </ion-item>
+                            <ion-item-options>
+                                <ion-item-option color="warning">
+                                    Reportar
+                                    <ion-icon :icon="warning" slot="end"></ion-icon>
+                                </ion-item-option>
+                            </ion-item-options>
+                        </ion-item-sliding>
+                    </template>
+                    <template v-else>
+                        <ion-item>
+                            <ion-label>No existen registros</ion-label>
+                            <ion-icon slot="end" :icon="sad"></ion-icon>
+                        </ion-item>
+                    </template>
+
                 </ion-list>
                 <ion-refresher slot="fixed" @ionRefresh="handleRefresh($event)">
                     <ion-refresher-content></ion-refresher-content>
@@ -32,22 +54,24 @@
 </template>
 
 <script lang="ts" setup>
-import { IonPage, IonHeader, IonToolbar, IonLabel, IonContent, IonButtons, IonMenuButton, IonRefresher, IonRefresherContent, IonCard, IonCardContent, IonItem, IonDatetimeButton, IonModal, IonDatetime, IonList, IonButton, IonIcon, IonPopover } from '@ionic/vue';
+import { IonPage, IonHeader, IonToolbar, IonLabel, IonContent, IonButtons, IonMenuButton, IonRefresher, IonRefresherContent, IonItem, IonList, IonButton, IonIcon, IonChip, IonItemSliding, IonItemOptions, IonItemOption } from '@ionic/vue';
 import MainMenu from '@/components/MainMenu.vue';
 import FilterModal from './FilterModal.vue';
-import { filter } from 'ionicons/icons';
+import { filter, sad, warning } from 'ionicons/icons';
 import { onBeforeMount, ref } from 'vue';
 
 import moment from 'moment';
-import { OdooResponse } from '@/models/models';
-import { http } from '@/common';
 
+moment.locale('es', {
+    weekdays: ['Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado', 'Domingo']
+})
+
+import { http, showLoading } from '@/common';
 
 // Hooks
 onBeforeMount(() => {
     getAttendances();
 })
-
 
 function handleRefresh(event: any) {
 
@@ -61,11 +85,15 @@ function handleRefresh(event: any) {
 // Local Store
 
 var showFilterModal = ref(false);
-const attendaces = ref<any>([]);
+const attendances = ref<any>([]);
 
 
-function setFilters(dates_modal: any) {
+async function setFilters(dates_modal: any) {
     showFilterModal.value = false;
+
+    if (!dates_modal) {
+        return;
+    }
 
     dates.value.startDate = dates_modal.dates.startDate;
     dates.value.endDate = dates_modal.dates.endDate;
@@ -81,15 +109,38 @@ const dates = ref({
 
 
 async function getAttendances() {
+    const loading = await showLoading({ message: 'Cargando datos...' })
     const response = await http({
         endpoint: 'racetime/api/marcaciones', args: {
             dates: dates.value
         }
     });
-    console.log(response.data);
 
-    attendaces.value = [...response.data.result]
+    attendances.value = response.data.result.map((d: any) => {
+        d.records.forEach((rec: any) => {
+            rec.fecha_hora = moment(rec.fecha_hora).subtract(5, 'hours').format('HH:mm:ss');
+        });
+        d.day = moment(d.fecha).format('dddd');
+        return d
+    });
+
+    loading.dismiss();
 }
 
 
 </script>
+
+<style lang="scss" scoped>
+.item-attendance {
+    margin-bottom: 0;
+
+    & li {
+        margin-left: 2rem;
+        font-size: 14px;
+    }
+}
+
+.attendances {
+    margin-top: 0;
+}
+</style>
